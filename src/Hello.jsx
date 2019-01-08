12433963c9abc6b63dcb52067b1266c9dd760195
Jonathan <jon1969edwards@gmail.com>
2019-01-08 19:41:06 +0100

    Adds images to 'my projects' and 'landing page'

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,9 +1,15 @@
-import React from "react"
+import React from 'react';
+import LazyLoad from 'react-lazy-load';
 
-const Hello = () => {
-    return (
-        <h1 className="content-text">DIE</h1>
-    )
-}
+const Hello = () => (
+  <div class="footer-content">
+
+    <div className="filler" />
+    <LazyLoad height={762} offsetVertical={300}>
+      <img src='https://arkyon.in/wp-content/uploads/2018/07/68971260-programmer-wallpapers-1024x577.png' />
+    </LazyLoad>
+   
+  </div>
+);
 
 export default Hello
